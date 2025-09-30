14f0b2bfba97a59bfcac6c7bd1665cca8e587963
debracarino <dcarino@clackamas.edu>
2025-09-30 13:37:19 -0700

    Removed node_modules from repository updates

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 /** server.js
  *  demonstration of simple, custom web server using Express.js
  * 
- *  Debra Carino
- *  9/29/2025
+ *  Your Name Here
+ *  CS 233W - Week 1 Activity
+ *  10/1/2025
  */
 
 //import desired frameworks
 const express = require('express');
 const app = express();
+
+//set listening port value
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//configure routing
+app.get('/', (request, response) => {
+  response.send('<h1>The Tasty Tortellini</h1><h2>Southeast PDX\'s favorite Italian bistro</h2>');
 });
 
+app.get('/menus', (request, response) => {
+  response.send('<h1>Menus</h1><ul><li>Lunch</li><li>Dinner</li><li>Drinks</li><li>Desserts</li></ul>');
+});
+
+//connect server to desired port
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log("Web server currently listening on port", PORT);
 });
